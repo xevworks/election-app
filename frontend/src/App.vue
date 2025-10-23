c9f66bbfe11a9fda5cbfe01b7dbeedc44d6409ef
xevworks <alqaaf@lifematics.co.jp>
2025-10-24 01:18:04 +0900

    feat: Update app bar title to include logo and improve branding

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <v-app>
     <v-app-bar density="comfortable" elevation="1">
-      <v-app-bar-title>KPU | PPI Osaka-Nara</v-app-bar-title>
+      <v-app-bar-title>
+        <img src="https://osakanara.ppijepang.org/wp-content/uploads/sites/5/2023/12/Logo-2.svg" alt="PPI Osaka-Nara Logo" style="height: 40px; vertical-align: middle;" />
+        KPU
+      </v-app-bar-title>
       <v-spacer />
       <v-btn variant="text" :to="{ name: 'home' }">Beranda</v-btn>
       <v-btn variant="text" :to="{ name: 'vote' }">Vote</v-btn>
