fd1f2d8a776cc9fc6c73379c95bae859c49c450c
xevworks <alqaaf@lifematics.co.jp>
2025-10-23 17:52:22 +0900

    feat: Update database configuration to use dj_database_url and enhance nginx configuration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import environ
 import os
 from corsheaders.defaults import default_headers, default_methods
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -107,11 +108,19 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
