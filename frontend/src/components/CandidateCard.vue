0bb4cef5ccba8b20e9f0005bf86c7cc5981334c9
xevworks <alqaaf@lifematics.co.jp>
2025-10-23 23:22:50 +0900

    feat: Improve CandidateCard subtitle with expandable functionality and button for long programs

--- frontend/src/components/CandidateCard.vue
+++ frontend/src/components/CandidateCard.vue
@@ -4,14 +4,24 @@
     <v-card-title class="text-h6 pt-3">{{ name }}</v-card-title>
     <v-card-subtitle 
       class="pb-1 cursor-pointer" 
-      :class="{ 'line-clamp-2': !expanded }"
-      @click="expanded = !expanded"
+      :class="{ 'line-clamp-2': !expanded && program.length > 80 }"
+      @click="toggleExpand"
     >
       {{ program }}
-      <v-icon v-if="program.length > 80" size="small" class="ml-1">
+    </v-card-subtitle>
+    <v-btn 
+      v-if="program.length > 80"
+      variant="text" 
+      size="x-small" 
+      color="primary"
+      @click="toggleExpand"
+      class="mt-n2 mb-2"
+    >
+      {{ expanded ? 'Sembunyikan' : 'Selengkapnya' }}
+      <v-icon size="small" class="ml-1">
         {{ expanded ? 'mdi-chevron-up' : 'mdi-chevron-down' }}
       </v-icon>
-    </v-card-subtitle>
+    </v-btn>
     <v-card-text class="text-body-2"><slot /></v-card-text>
   </v-card>
 </template>
@@ -22,6 +32,10 @@ import { ref } from 'vue'
 defineProps({ name: String, program: String, posterUrl: String })
 
 const expanded = ref(false)
+
+const toggleExpand = () => {
+  expanded.value = !expanded.value
+}
 </script>
 
 <style scoped>
@@ -31,10 +45,12 @@ const expanded = ref(false)
   -webkit-box-orient: vertical;
   overflow: hidden;
   text-overflow: ellipsis;
+  line-height: 1.5rem;
 }
 
 .cursor-pointer {
   cursor: pointer;
+  user-select: none;
 }
 
 .cursor-pointer:hover {
